0e14c3a2e4e93d8edcf807385252a7a883687e78
MrChanghao <lnl2048@gmail.com>
2021-06-09 19:21:22 +0900

    linked list 정리

--- data_structure/linked_list_cycle.py
+++ data_structure/linked_list_cycle.py
@@ -7,9 +7,62 @@ linked list가 연결 되어 있는지 체크
 
 
 class Node(object):
-    def __init__(self, value, next):
+    def __init__(self, value):
         self.value = value
         self.next = None
+
+
+# inserting node
+
+class LinkkedList(object):
+    def __init__(self):
+        self.head = None
+
+    # O(1)
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def append(self, new_data):
+        new_node = Node(new_data)
+        if(self.head is None):
+            self.head = new_node
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+
+        last.next = new_node
+
+    def insertAfter(self, prev_node, new_data):
+        if prev_node is None:
+            print('previous node must in liknked list')
+            return
+
+        new_node = Node(new_data)
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+
+
+    def reverse(self):
+        prev = None
+        current = self.head
+        while current is not None:
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        self.head = prev
+
+    def print_list(self):
+        temp = self.head
+        while temp:
+            if temp.value != None:
+                print(temp.value, end=' --> ')
+
+
+            temp = temp.next
 
 
 def hasCycle(self, head):
@@ -29,4 +82,13 @@ def hasCycle(self, head):
     return False
 
 
-print(hasCycle([1, 2]))
+
+
+list = LinkkedList()
+
+list.append(4)
+list.append(1)
+list.append(2)
+list.push(5)
+list.push(10)
+list.print_list()
